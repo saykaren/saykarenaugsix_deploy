63133e6d6b5bf236e5b90768da823f148d5222da
Karen Garcia <karen.garcia@nexscreen.com>
2019-08-06 12:48:00 -0600

    changed homepage from the generic to saykaren.com

---
+++ precache-manifest.5108fba2d3c4ab76c9982e68b7272339.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff15eb75c7722d9186900c73ab7af70f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1741d1719bd32549b347",
+    "url": "/static/css/main.8cf572a4.chunk.css"
+  },
+  {
+    "revision": "e8f50145bd839aff1fa2",
+    "url": "/static/js/2.2c9dc542.chunk.js"
+  },
+  {
+    "revision": "1741d1719bd32549b347",
+    "url": "/static/js/main.51c525f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7a737d02eb03b76e030334a473132d90",
+    "url": "/static/media/Bomdigity.7a737d02.gif"
+  },
+  {
+    "revision": "2099b4832a54e318deaaa2908f793138",
+    "url": "/static/media/Karen_freeCodeCamp_Certificate_Responsive_Web_Design_2.13.2019.2099b483.png"
+  },
+  {
+    "revision": "7f28071e5bee2a6f88689506534d2d30",
+    "url": "/static/media/Number_Guesser.7f28071e.gif"
+  },
+  {
+    "revision": "56952159168e1c475dc1edeb1a7be781",
+    "url": "/static/media/React_Website_ES6_8.1.2019.56952159.png"
+  },
+  {
+    "revision": "230b6c8b8f16c40b30484e727a311da5",
+    "url": "/static/media/checkwritting.230b6c8b.png"
+  },
+  {
+    "revision": "bd82fb9a7ee7894d890bdf0d6bedc8bf",
+    "url": "/static/media/flashcard-starter.bd82fb9a.gif"
+  },
+  {
+    "revision": "f00add6dbcf78651995998cfebba726f",
+    "url": "/static/media/karenHero.f00add6d.jpg"
+  },
+  {
+    "revision": "4ee98378ff2cd65d37dc47e7b6cd0023",
+    "url": "/static/media/sayKaren_Logo_transparent_7.2019.4ee98378.png"
+  },
+  {
+    "revision": "d0827d01ac17b128172a9f867b8d0c82",
+    "url": "/static/media/sayKaren_logo-white_8.1.2019.d0827d01.png"
+  }
+]);
